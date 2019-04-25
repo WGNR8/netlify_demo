9d87c1a08a98f5915780cb9fa470ec6d06d1de90
Wagner Mattos <wagner_mattos@msn.com>
2019-04-25 18:14:19 -0300

    feat: call proxy and show list from json response

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,4 +5,18 @@ const getHello = async () => {
     document.getElementById('random-hello').innerText = hello;
 }
 
+const getUsers = async () => {
+    const res = await fetch('/api/users');
+    const users = await res.json();
+
+    users.forEach(user => {
+        const li = document.createElement('li');
+        const item = user.name + ' : [' + user.email + ']';
+        const text = document.createTextNode(item);
+        li.appendChild(text);
+        document.getElementById('users').appendChild(li);
+    });
+}
+
 getHello();
+getUsers();
